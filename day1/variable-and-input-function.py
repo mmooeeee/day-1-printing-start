22b1de0abe4b68144835c06439f04264b3fa5f3c
Moe <43162722+mnajamudin28@users.noreply.github.com>
2022-03-21 15:10:16 +0700

    Add files via upload

---
+++ day1/variable-and-input-function.py
@@ -0,0 +1,40 @@
+#input function
+input("What is your name?")
+print("Helloo" + input ("what is your name? "))
+
+#Variable
+name = "Jack"
+print (name)
+
+name = "Angela"
+print (name)
+
+#check length string
+print ( len (input("What is your name? ") ) ) #or
+name = input("What is your name? ")
+length = len(name)
+print (length)
+
+# excercise variable
+name = input("what is your name? ")
+length = len(name)
+print = (length)
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a
+a = b
+b = c
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
